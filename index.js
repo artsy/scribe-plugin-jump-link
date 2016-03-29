3f9fb8c169cd5fc9ffe3a30cffdba93490b56078
kanaabe <kanakoabe5@gmail.com>
2016-03-29 14:59:17 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // https://github.com/guardian/scribe-plugin-link-prompt-command
 //
 (function() {
-
   // The main plugin function
   var scribePluginJumpLink = function () {
     return function (scribe) {
@@ -15,13 +14,29 @@
       jumpLinkCommand.execute = function () {
         var selection = new scribe.api.Selection(),
             that = this;
-        console.log(selection);
+        var fullString = selection.selection.baseNode.textContent
+        if($(selection.selection.baseNode.parentNode).hasClass('is-jump-link')){
+          // Remove the jump link
+          $(selection.selection.baseNode).unwrap()
+        }else{
+          // Create the jump link
+          var startOffset = selection.range.startOffset
+          var numOfChar = selection.range.endOffset - startOffset
+          var replace = fullString.substr(startOffset, numOfChar)
+          replace = "<a class='is-jump-link' name=" + replace + ">" + replace + '</a>'
+          var newHtml = splice(fullString, startOffset, numOfChar, replace)
+          $(selection.selection.baseNode).replaceWith(newHtml)
+        }
       };
 
-      // Set this as the jump link plugin
+      // Set this as the linking plugin
       scribe.commands.jumpLink = jumpLinkCommand;
     };
   };
+
+  var splice = function(str, start,length,replacement) {
+    return str.substr(0,start)+replacement+str.substr(start+length);
+  }
 
   // Export for CommonJS & window global. TODO: AMD
   if (typeof module != 'undefined') {
